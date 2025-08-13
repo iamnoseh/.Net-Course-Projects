bd7aedce663e55bd39254ad32cc21f795a555cc2
Noseh Taghaymurodzoda <nosehtagaymurodzoda@gmail.com>
2025-08-13 11:40:11 +0500

    .

--- LIbraryManagementSystem/Infrastructure/Data/DataContext.cs
+++ LIbraryManagementSystem/Infrastructure/Data/DataContext.cs
@@ -2,7 +2,7 @@
 namespace Infrastructure.Data;
 public class DataContext
 {
-    private string connectionString = "Server=localhost;Database=library-management-db;Username=postgres;Password=12345;Include Error Detail=true";
+    private string connectionString = "Server=localhost;Database=library-management-db;Username=postgres;Password=12345;";
     public NpgsqlConnection GetConnection()
     {
         return new NpgsqlConnection(connectionString);
